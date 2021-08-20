039eb1e553a32f1ab85ecc4fc3e766ffb0e35a87
Joao Rocha <joaogsrocha@gmail.com>
2021-08-20 03:23:31 +0100

    [Add][1st] Console Program

---
+++ 01_Console/src/com/joaogsrocha/Main.java
@@ -0,0 +1,22 @@
+package com.joaogsrocha;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        InputStreamReader r = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(r);
+
+        String name = "";
+        while (!name.equals("stop")){
+            System.out.println("Enter data:");
+            name = br.readLine();
+            System.out.println("data is:" + name);
+        }
+        br.close();
+        r.close();
+    }
+}
